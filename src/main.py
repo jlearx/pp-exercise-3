d4993c6cf69c02abb8eeea1d7c26bec222fb875e
U-AMR\jlearx <jlearx@jlearx-MOBL1.amr.corp.intel.com>
2017-09-07 10:16:12 -0700

    Added one-line solution and new list solution

--- src/main.py
+++ src/main.py
@@ -6,3 +6,17 @@ Created on Sep 7, 2017
 
 if __name__ == '__main__':
     a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+    b = []
+    
+    # One line solution with list comprehension
+    [print(n) for n in a if (n < 5)]
+    
+    # New list solution
+    for n in a:
+        if (n < 5):
+            b.append(n)
+    
+    print(b)
+    
+    
+    
